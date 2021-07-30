a112a8c612b13ac93e9fd2ff6aea60b2419fd650
Ha_LN <ha_ln@EVN084-HaLN.local>
2021-07-30 15:23:09 +0700

    react-ts-login-google-ontab

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+declare var google: any;
 
 function App() {
+  const [isSignIn, setSignIn] = useState<boolean>(false)
+  useEffect(() => {
+    const handleCredentialResponse = (response: any) => {
+      console.log(response);
+    };
+    const client_id = process.env.REACT_APP_CLIENT_ID
+    const callback = handleCredentialResponse;
+    const auto_select = false;
+    console.log(google.accounts.id.initialize)
+    google.accounts.id.initialize({ client_id, callback, auto_select });
+    google.accounts.id.prompt((notification: any) => {
+      console.log(notification);
+    });
+    console.log(setSignIn)
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+      isSignIn ?
+        <div>You are signIn</div> :
+        <div>You are not signIn</div>
+      }
     </div>
   );
 }
